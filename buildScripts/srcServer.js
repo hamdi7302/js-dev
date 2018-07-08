f8e892cf717d5a0427ac43e849c34a218d281a5a
hamdi7302 <hamdi.bntiba@gmail.com>
2018-07-08 20:29:56 +0100

    wifi check

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,20 @@
-var express = require('express')
-var path = require('path')
-var open = require('open')
+import express from 'express'
+import path from 'path'
+import open from  'open'
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
 
 var port = 8080
 var app = express()
 
+//config webapck to express
+var compiler =webpack(config)
+
+app.use(require('webpack-dev-middleware')(compiler,{
+    noinfo:true,
+    PublicPath: config.output.PublicPath
+}))
+//
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,'../src/index.html'))
 })
