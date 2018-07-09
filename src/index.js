331370c1d50418e7693e689618505be7e28136f2
hamdi7302 <hamdi.bntiba@gmail.com>
2018-07-09 01:44:18 +0100

    monday 1:44

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import numeral from 'numeral'
+import  './index.css'
+
+/*eslint-disable no-console */
+
+const courseValue = numeral(1000).format('0,0.00');
+ 
+console.log(`pay me ${courseValue}`);
+
